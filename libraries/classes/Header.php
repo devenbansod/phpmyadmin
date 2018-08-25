c1a2abeb898e043cd4e0a33963de55a3586342f1
Bansod, Deven <debansod@paypal.com>
2018-08-25 10:57:45 +0530

    Some more javascript files structured properly.
    Shortcut handler and page settings js files added in modular code.
    
    Signed-Off-By: Piyush Vijay <piyushvijay.1997@gmail.com>

--- libraries/classes/Header.php
+++ libraries/classes/Header.php
@@ -215,8 +215,9 @@ class Header
         if ($GLOBALS['cfg']['enable_drag_drop_import'] === true) {
             $this->_scripts->addFile('drag_drop_import.js');
         }
+        $this->_scripts->addFile('page_settings');
         if (! $GLOBALS['PMA_Config']->get('DisableShortcutKeys')) {
-            $this->_scripts->addFile('shortcuts_handler.js');
+            $this->_scripts->addFile('shortcuts_handler');
         }
         $this->_scripts->addCode($this->getJsParamsCode());
     }
